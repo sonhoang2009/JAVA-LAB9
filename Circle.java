06cbd16d6dccf52150d597f404457cef38db9b0a
sonhoang2009 <65003120+sonhoang2009@users.noreply.github.com>
2020-10-24 19:49:53 +0700

    VD1
    VD1

---
+++ Circle.java
@@ -0,0 +1,23 @@
+package lab9.vd1;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea(){
+        return radius * radius * Math.PI;
+    }
+
+    public String toString(){
+        return  "Circle [ radius = "+ radius + "]";
+    }
+
+
+}
